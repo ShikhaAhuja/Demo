53904605e3006655f80912fa9f7b0c993ed1f75b
ShikhaAhuja <shikhaahuja505@gmail.com>
2017-12-26 22:50:07 +0530

    Add files via upload

---
+++ homeController.js
@@ -0,0 +1,20 @@
+﻿(function (homeController) {
+    var data = require("../data");
+
+    homeController.init = function (app) {
+
+        app.get("/", function (req, res) {
+
+            data.getNoteCategories(function (err, results) {
+
+                res.render("index", { title: "Express + Vash" , error : err , categories : results});
+
+            });
+
+            //res.send("<html><body><h1>Express</h1></body></html>");
+            //res.render("jade/index", { title : "Express + Jade" });
+            
+        });
+
+    };
+})(module.exports);
